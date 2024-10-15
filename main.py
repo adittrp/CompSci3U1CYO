eacf796288b479b2a5454a7ab1a74e378a4a0e51
adittrp <166170305+adittrp@users.noreply.github.com>
2024-10-15 16:38:48 -0500

    push

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import pygame
 pygame.init()
 
+left_edge = 100
+right_edge = 100
+width = 500
+length = 500
 
 window = pygame.display.set_mode((1280, 920))
 window.fill((100, 100, 100))
 pygame.display.flip()
+
+pygame.draw.rect(window, "black", (left_edge, right_edge, width, length))
 
 running = True
 while running:
