d732c34109d4b276f76b63da0d5275795c5a02f2
adittrp <166170305+adittrp@users.noreply.github.com>
2024-10-15 13:16:18 -0500

    push

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import pygame
+pygame.init()
 
-print("Hello World hello")
+
+window = pygame.display.set_mode((1280, 920))
+window.fill((100, 100, 100))
+pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.flip()
+
+
+pygame.quit()
